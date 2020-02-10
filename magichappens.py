d744880f210c7828ff932eab4567994fb1f47aac
jjjilot <45500145+jjjilot@users.noreply.github.com>
2020-02-10 12:59:53 -0800

    making sure i can add stuff

---
+++ magichappens.py
@@ -0,0 +1,14 @@
+#import requests
+#from bs4 import beautifulsoup4
+import time
+
+def magic():
+    oldtext = "i am walker and also run"
+    newtext = "i am walker and never run"
+
+    if (oldtext == newtext):
+        print (0)
+    else:
+        print (newtext)
+
+magic()
