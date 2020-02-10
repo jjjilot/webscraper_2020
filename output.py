285ff30aa34d4136f9ffd1c95ffe5e00bca5122e
Henry McCreery <henrymccy@gmail.com>
2020-02-10 12:34:51 -0800

    stored all possible variables within send_text() function

--- output.py
+++ output.py
@@ -3,19 +3,18 @@ from output_variables import *
 from datetime import time
 from datetime import datetime
 
-client = Client(account_sid, auth_token)
 # client.messages.create(body="test text",to=my_phone_number,from_=twilio_phone_number)
 
-now = datetime.now()
-dt_string = now.strftime("%H:%M")
-dt_hour = now.strftime("%H")
-if int(dt_hour)<12:
-    am_pm = 'in the morning'
-else:
-    am_pm = 'in the afternoon'
-message = 'The current time is ' + str(dt_string) + ' ' + am_pm + '.'
-
 def send_text():
+    client = Client(account_sid, auth_token)
+    now = datetime.now()
+    dt_string = now.strftime("%H:%M")
+    dt_hour = now.strftime("%H")
+    if int(dt_hour)<12:
+        am_pm = 'in the morning'
+    else:
+      am_pm = 'in the afternoon'
+    message = 'The current time is ' + str(dt_string) + ' ' + am_pm + '.'
     # message_to_send = str(input('Message to send: '))
     client.messages.create(body=message,to=my_phone_number,from_=twilio_phone_number)
 
