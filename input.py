c9db8f616ca1cd69eeba3267e56e611006845c71
AeoTheAstronaut <45434833+AeoTheAstronaut@users.noreply.github.com>
2020-02-25 13:03:54 -0800

    Update variables

--- input.py
+++ input.py
@@ -9,9 +9,9 @@ def main():
   mod_url = 'https://csmoodle.clevelandhighschool.org/'
   html_text = requests.get(mod_url).text
   soup = BeautifulSoup(html_text, 'html.parser')
-  result = soup.find(id='docs-internal-guid-87d80929-7fff-0746-c3be-a3d1179550a7').text
+  newtext = soup.find(id='docs-internal-guid-87d80929-7fff-0746-c3be-a3d1179550a7').text
 
-  return(result)
+  return(newtext)
 
 if __name__ == "__main__": 
   main();
